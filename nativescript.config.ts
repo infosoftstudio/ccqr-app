703babfb04a787f79f5931d64c0afd0bd22f1769
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-04-17 17:45:47 +0800

    a

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.tapatgensan',
+  id: 'org.nativescript.ccqrapp',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
